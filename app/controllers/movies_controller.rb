bb6fc8f8c0d2f24aec891439e7c324844b0da223
Minh Luong <minh.luong@berkeley.edu>
2012-03-16 16:29:31 -0700

    Test filtering v1.1.12

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,9 +10,9 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.all_ratings
     filtered_ratings = ["G", "P"] # @all_ratings - params[:ratings].keys
     if (params[:sort] == "title")
-      @movies = Movie.find(:all, :order => "title").where(:rating => filtered_ratings)
+      @movies = Movie.find(:all, :conditions => {:rating => filtered_ratings}, :order => "title")
     elsif (params[:sort] == "release_date")
-      @movies = Movie.find(:all, :order => "release_date").where(:rating => filtered_ratings)
+      @movies = Movie.find(:all, :conditions => {:rating => filtered_ratings}, :order => "release_date")
     elsif (params[:sort] == nil)
       @movies = Movie.find(:all, :conditions => {:rating => filtered_ratings}) #.where(:rating => filtered_ratings)
     end
