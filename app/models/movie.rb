a7e0ab36c6e17133f513c6d389261f04c6ff7cb2
Minh Luong <minh.luong@berkeley.edu>
2012-03-16 14:01:42 -0700

    Sorting title and release_date v1.11.1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,16 @@
 class Movie < ActiveRecord::Base
   class << self
-    attr_accessor :all_ratings
+    attr_writer :all_ratings
+  end
+  def all_ratings
+    if (@all_ratings == nil)
+      @all_ratings = ["G", "PG-13", "R", "PG"]
+    end
+    return @all_ratings
   end
   def rating=(value)
     if (@all_ratings == nil)
-      @all_ratings = []
+      @all_ratings = ["G", "PG-13", "R", "PG"]
     end
     if (@all_ratings.find_index(value) == nil)
       all_ratings.push(value)
