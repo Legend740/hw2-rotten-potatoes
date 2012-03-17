e68da11c1074f55278cb0d3fe5d1352bb5e3ba3d
Minh Luong <minh.luong@berkeley.edu>
2012-03-17 02:03:36 -0700

    Test filtering v1.27

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,7 +9,7 @@ class MoviesController < ApplicationController
   def index
     @all_ratings = Movie.all_ratings
     if (params[:filter] != nil)
-      @filtered_ratings = params[:filter].scan(/[\w-]+/)
+      @filtered_ratings = params[:filter].gsub(/%22/, "\"").gsub(/%22/, "\"").gsub(/%5B/, "[").gsub(/%5D/, "]").gsub(/%2C/, ",").scan(/[\w-]+/)
       if (@filtered_rating != nil and @filtered_rating.length == 4)
         @filtered_ratings = []
       end
