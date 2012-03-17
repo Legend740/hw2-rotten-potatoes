015cea83ddec6c7825b3a91408d0aed654f3e47e
Minh Luong <minh.luong@berkeley.edu>
2012-03-17 02:19:23 -0700

    Test filtering v1.31

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,22 +13,20 @@ class MoviesController < ApplicationController
     else
       @filtered_ratings = params[:ratings] ? params[:ratings].keys : []
     end
-    puts "[LOOKKKKKK]"
-    puts @filtered_ratings
     if (params[:sort] == "title") # Sort by titles
-      if (params[:ratings]) # filter ratings
+      if (params[:ratings] or params[:filter]) # filter ratings
         @movies = Movie.find(:all, :conditions => {:rating => @filtered_ratings}, :order => "title")
       else
         @movies = Movie.find(:all, :order => "title")
       end
     elsif (params[:sort] == "release_date") # Sort by release_date
-      if (params[:ratings]) # filter ratings
+      if (params[:ratings] or params[:filter]) # filter ratings
         @movies = Movie.find(:all, :conditions => {:rating => @filtered_ratings}, :order => "release_date")
       else
         @movies = Movie.find(:all, :order => "release_date")
       end
     elsif (params[:sort] == nil)
-      if (params[:ratings]) # filter ratings
+      if (params[:ratings] or params[:filter]) # filter ratings
         @movies = Movie.find(:all, :conditions => {:rating => @filtered_ratings})
       else
         @movies = Movie.all
